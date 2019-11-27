bb51dc18ff869a2410f47882c80e597c7296daa4
oevasquez832 <oevasquez832@gmail.com>
2019-11-26 18:57:33 -0600

    committing all components of this project

---
+++ README.md
@@ -0,0 +1,40 @@
+# Project-two
+Project Two
+
+And the Winner Goes to…..
+
+Create an interactive Dashboard to display recent and historical Academy Award Winners.  User will be able to select years, categories and details for the following catagories.
+
+Best Movie,
+Best Actor,
+Best Actress,
+Best Director
+
+Dataset to include:
+
+Current and Past Winners,
+Movie details- Genre, Plot, Gross,
+Actor/Actress details
+
+Since the Academy Awards website does not have an API.  We plan to scrape the data from various websites:
+
+Wikipedia,
+Omdb,
+Imdb,
+Cinemasight,
+Kaggle
+
+Landing Page with links to four catagories
+Each catagory page will have 3 pages
+  1) Graphs - Pie for genre / Bubble for Budget vs Rev / Bar - Ratings
+  2) Map - 
+  3) Data
+
+
+
+Team Members
+  Solomon,
+  Dan,
+  Oscar,
+  Kenneth,
+  Kathleen
---
+++ logic.js
@@ -0,0 +1,349 @@
+
+
+// An array containing each city's name, location, and Actor
+var cities = [{
+  location: [51.509865, -0.118092],
+  location1: [51.509865, -0.118092],
+  location2: [51.509865, -0.118092],
+  name: "New Cross, London",
+  name1: "New Cross, London",
+  name2: "New Cross, London",
+  Actor: "Gary Oldman",
+  Actor1: "Gary Oldman",
+  Actor2: "Gary Oldman"
+},
+{
+  location: [41.5532, -70.6086],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+},
+{
+  location: [34.0928, -118.3287],
+  location1: [34.0928, -118.3287],
+  location2: [34.0928, -118.3287],
+  name: "Hollywood",
+  name1: "Hollywood",
+  name2: "Hollywood",
+  Actor: "Leonardo DiCaprio",
+  Actor1: "Leonardo DiCaprio",
+  Actor2: "Leonardo DiCaprio"
+},
+{
+  location: [51.4975, -0.1357],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+},
+{
+  location: [29.2097, -99.7862],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+},
+{
+  location: [51.5074, -0.1278],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+},
+{
+  location: [48.8285, 2.2188],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+},
+{
+  location: [51.0577, 1.3081],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+},
+{
+  location: [34.0522, -118.2437],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+},
+{
+  location: [34.0195, -118.2437],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+},
+{
+  location: [51.5074, -0.1278],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+},
+{
+  location: [32.5007, -94.7405],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+},
+{
+  location: [43.0987, -77.4419],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+},
+{
+  location: [32.736, -96.2753],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+},
+{
+  location: [34.0195, -118.4912],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+},
+{
+  location: [42.139, -83.2418],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+},
+{
+  location: [40.9126, -73.8371],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+},
+{
+  location: [-41.2865, 174.7762],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+},
+{
+  location: [40.7489, -74.261],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+},
+{
+  location: [43.7711, 11.2486],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+},
+{
+  location: [40.2091, -74.0386],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+},
+{
+  location: [-27.5598, 151.9507],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+},
+{
+  location: [33.7701, -118.1937],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+},
+{
+  location: [37.978, -122.0311],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+},
+{
+  location: [40.8116, -73.9465],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
+}
+                  
+];
+
+var myIcon = L.icon({
+    iconUrl: 'oscar.png',
+    iconSize: [38, 95],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+});
+
+var actormarker = []
+var actressmarker = []
+var directormarker = []
+
+// Loop through the cities array and create one marker for each city, bind a popup containing its name and Actor add it to the map
+for (var i = 0; i < cities.length; i++) {
+  var city = cities[i];
+  actormarker.push(L.marker(
+      city.location, {
+      icon: myIcon})
+    .bindPopup("<h1>" + city.name + "</h1> <hr> <h3>Actor " + city.Actor + "</h3>"))
+  actressmarker.push(L.marker(
+      city.location1, {
+      icon: myIcon})
+    .bindPopup("<h1>" + city.name1 + "</h1> <hr> <h3>Actress " + city.Actor1 + "</h3>"))
+  directormarker.push(L.marker(
+      city.location2, {
+      icon: myIcon})
+    .bindPopup("<h1>" + city.name2 + "</h1> <hr> <h3>Director " + city.Actor2 + "</h3>"))
+}
+
+// Add a tile layer
+// Add a tile layer
+var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?" +
+"access_token=pk.eyJ1Ijoic29sb21vbm1pbGxlciIsImEiOiJjamd4MDduZ2QxdjNtMnFtcWticW4yZDdrIn0.UzUscd_xQuIDVRDmh2CZjw"
+);
+
+// Define variables for our base layers
+var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
+"access_token=pk.eyJ1Ijoic29sb21vbm1pbGxlciIsImEiOiJjamd4MDduZ2QxdjNtMnFtcWticW4yZDdrIn0.UzUscd_xQuIDVRDmh2CZjw"
+);
+var Actor = L.layerGroup(actormarker);
+var Actress = L.layerGroup(actressmarker);
+var Director = L.layerGroup(directormarker);
+
+
+// Create a baseMaps object
+var baseMaps = {
+  "Street Map": streetmap,
+  "Dark Map": darkmap
+};
+
+// Create an overlay object
+var overlayMaps = {
+  "Actor Home": Actor,
+  "Actress Home": Actress,
+  "Director Home": Director
+};
+
+// Create a map object
+var myMap = L.map("map", {
+  center: [37.983810, 23.727539],
+  zoom: 2,
+  layers: [darkmap, Actor, Actress, Director]
+});
+
+// Pass our map layers into our layer control
+// Add the layer control to the map
+L.control.layers(baseMaps, overlayMaps, {
+  collapsed: false
+}).addTo(myMap);
